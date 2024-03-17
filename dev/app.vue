d8f596d434a34bad435c12029b6c69eeceed9476
Mohamed Hazem <125741147+mohamed-hazem-fathy@users.noreply.github.com>
2024-03-18 01:12:37 +0200

    Fix dev bug

--- dev/app.vue
+++ dev/app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
   </div>
 </template>
+
+<style>
+body {
+  box-sizing: border-box;
+  height: 3000000px ;
+}
+
+</style>
---
+++ dev/components/Loading.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-h1">Loading</h1>
+    </div>
+</template>
+
+<script>
+export default defineComponent({
+
+});
+</script>
