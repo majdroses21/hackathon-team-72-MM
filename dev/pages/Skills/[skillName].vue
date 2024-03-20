f5e5867e17e2a661ac9f06c76fe814933fd18248
majdroses21 <www.joooooooooo.majd@gmail.com>
2024-03-20 03:05:10 +0300

    Adding new pages

---
+++ dev/pages/Skills/[skillName].vue
@@ -0,0 +1,130 @@
+<template>
+    <v-container>
+        <v-img class="align-end" height="200" src="/images/code.jpg" cover></v-img>
+    </v-container>
+
+    <v-container>
+        <v-row>
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+            <v-col lg="2" md="3" sm="4" xs="4">
+                <v-card class="mx-auto" max-width="344">
+                    <v-img height="200px" src="/images/js.png" cover></v-img>
+
+                    <v-card-title class=" text-center ">
+                        <h3> Java Script</h3>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+            <!--  -->
+        </v-row>
+    </v-container>
+</template>
