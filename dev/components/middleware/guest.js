db0ecf98454653824e67d15c4e7047c25c78eeab
majdroses21 <www.joooooooooo.majd@gmail.com>
2024-03-27 13:28:33 +0300

    no think new just fix conflect from my fils

---
+++ dev/components/middleware/guest.js
@@ -0,0 +1,27 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    // Guest + dashboard
+    // Guest + guest
+    // User + dashboard
+    // User + guest
+
+    const loggedIn = useState('loggedIn', ()=>false)
+
+    console.log('guest.js middleware says: loggedIn.value = ', loggedIn.value)
+
+    if(loggedIn.value) return navigateTo('/dashboard')
+
+
+    // const guestPages
+    // Only guest [login, register]
+    // Only user [dashboard]
+
+    // if (to.params.id === '1') {
+    //   return abortNavigation()
+    // }
+    // // In a real app you would probably not redirect every route to `/`
+    // // however it is important to check `to.path` before redirecting or you
+    // // might get an infinite redirect loop
+    // if (to.path !== '/') {
+    //   return navigateTo('/')
+    // }
+  })
---
+++ dev/components/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import { createVuetify } from 'vuetify'
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+export default defineNuxtPlugin((nuxt) => {
+    const vuetiyfy = createVuetify({
+        ssr: false,
+        components,
+        directives
+    });
+    nuxt.vueApp.use(vuetiyfy);
+})
+
+// export default {
+
+// }
